54f1411b78a1f483e0404bf6c280bb1ec8c1535a
kylecarey <d00222426@student.dkit.ie>
2021-04-15 10:42:03 +0100

    Test

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace EZPCBuilder.Controllers
             return View();
         }
 
+        public IActionResult KyleTest()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
