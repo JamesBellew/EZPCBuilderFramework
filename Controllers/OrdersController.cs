74ce18899c006e364ad8486b859f62adbea3bf42
kylecarey <d00222426@student.dkit.ie>
2021-05-03 14:07:25 +0100

    Order Functionality, can create and view an order.

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,162 @@
+﻿using EZPCBuilder.Data;
+using EZPCBuilder.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EZPCBuilder.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMongoCollection<Order> _orderCollection;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public OrdersController(IMongoClient client, UserManager<IdentityUser> userManager, ApplicationDbContext context)
+        {
+            var database = client.GetDatabase("ezpcbuilderdotnet");
+            _orderCollection = database.GetCollection<Order>("orders");
+            _userManager = userManager;
+            _context = context;
+        }
+
+        // GET: OrdersController
+
+        public ActionResult Index()
+        {
+            string user = _userManager.GetUserId(HttpContext.User);
+            List<Order> orders = _orderCollection.Find(s => s.UserID == user).ToList();
+            return View(orders);
+        }
+
+        // GET: OrdersController/Details/5
+        public ActionResult Details(string id)
+        {
+            ObjectId oid = ObjectId.Parse(id);
+            Order order = _orderCollection.Find(o => o.ID == oid).FirstOrDefault();
+            return View(order);
+        }
+
+        // POST: OrdersController/Create
+    
+        public async Task<ActionResult> Create()
+        {
+            try
+            {
+                decimal orderTotal = 0;
+                List<PC> basketItems = new List<PC>();
+                string user = _userManager.GetUserId(HttpContext.User);
+
+                var basket = _context.Basket
+                    .Include(b => b.PC)
+                    .Include(b => b.User)
+                    .AsEnumerable().Where(b => b.UserID == user);
+
+                foreach (Basket b in basket)
+                {
+                    var pc = _context.PC
+                    .Include(p => p.Case)
+                    .Include(p => p.Graphics)
+                    .Include(p => p.Memory)
+                    .Include(p => p.Processor)
+                    .Include(p => p.Storage)
+                    .FirstOrDefaultAsync(m => m.ID == b.PCID);
+                    PC pC = await pc;
+                    pC.Quantity = b.Quantity;
+                    basketItems.Add(pC);
+
+                    orderTotal += pC.Price * pC.Quantity;
+                }
+
+                Order order = new Order();
+                order.ItemsOrdered = basketItems;
+                order.UserID = user;
+                order.OrderDate = DateTime.Now;
+                order.TotalCost = (double)orderTotal;
+
+                _orderCollection.InsertOne(order);
+
+                // Emptying Basket
+                foreach (Basket b in basket)
+                {
+                    _context.Basket.Remove(b);
+                }
+                await _context.SaveChangesAsync();
+
+               return RedirectToAction(nameof(Index));
+            } catch
+            {
+               return RedirectToAction(nameof(Index));
+            }
+        }
+
+        // GET: OrdersController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: OrdersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public async Task<ActionResult> Checkout()
+        {
+            decimal orderTotal = 0;
+            List<PC> basketItems = new List<PC>();
+            string user = _userManager.GetUserId(HttpContext.User);
+
+            var basket = _context.Basket
+                .Include(b => b.PC)
+                .Include(b => b.User)
+                .AsEnumerable().Where(b => b.UserID == user);
+
+            foreach (Basket b in basket)
+            {
+                var pc = _context.PC
+                .Include(p => p.Case)
+                .Include(p => p.Graphics)
+                .Include(p => p.Memory)
+                .Include(p => p.Processor)
+                .Include(p => p.Storage)
+                .FirstOrDefaultAsync(m => m.ID == b.PCID);
+                PC pC = await pc;
+                pC.Quantity = b.Quantity;
+                basketItems.Add(pC);
+
+                orderTotal += pC.Price;
+            }
+
+            Order order = new Order();
+            order.ItemsOrdered = basketItems;
+            order.UserID = user;
+            order.OrderDate = DateTime.Now;
+            order.TotalCost = (double)orderTotal;
+
+            return View(order);
+        }
+
+        public IActionResult Payment()
+        {
+            return View();
+        }
+    }
+}
