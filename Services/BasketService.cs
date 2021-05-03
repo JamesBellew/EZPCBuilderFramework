44b355067675535bc92fa42e19c9a016039f2254
kylecarey <d00222426@student.dkit.ie>
2021-05-03 18:27:08 +0100

    Order and Basket services added

---
+++ Services/BasketService.cs
@@ -0,0 +1,49 @@
+﻿using EZPCBuilder.Data;
+using EZPCBuilder.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EZPCBuilder.Services
+{
+    public class BasketService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BasketService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public BasketService()
+        {
+        }
+
+        // Gets the data from 
+        [HttpGet]
+        public IEnumerable<Basket> GetBasket(string user)
+        {
+            var basket = _context.Basket
+                    .Include(b => b.PC)
+                    .Include(b => b.User)
+                    .AsEnumerable().Where(b => b.UserID == user);
+            IEnumerable<Basket> baskets = basket;
+
+            return baskets;
+        }
+
+        // DELETE: Basket
+        public void DeleteBasket(IEnumerable<Basket> basket)
+        {
+            // Emptying Basket
+            foreach (Basket b in basket)
+            {
+                _context.Basket.Remove(b);
+            }
+             _context.SaveChanges();
+        }
+    }
+}
---
+++ Services/OrderService.cs
@@ -0,0 +1,68 @@
+﻿using EZPCBuilder.Data;
+using EZPCBuilder.Models;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EZPCBuilder.Services
+{
+    public class OrderService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMongoCollection<Order> _orderCollection;
+
+        public OrderService(ApplicationDbContext context, IMongoCollection<Order> orderCollection)
+        {
+            _context = context;
+            _orderCollection = orderCollection;
+        }
+
+        public OrderService()
+        {
+        }
+
+
+        // GET: PC from basket
+        private PC GetPCDetails(Basket basket)
+        {
+                var pc = _context.PC
+                .Include(p => p.Case)
+                .Include(p => p.Graphics)
+                .Include(p => p.Memory)
+                .Include(p => p.Processor)
+                .Include(p => p.Storage)
+                .FirstOrDefault(m => m.ID == basket.PCID);
+                PC PC = pc;
+                PC.Quantity = basket.Quantity;
+
+            return PC;
+        }
+
+        // CREATE: Order object
+        public static Order CreateOrder(IEnumerable<Basket> basket, string user)
+        {
+            OrderService os = new OrderService();
+            decimal orderTotal = 0;
+            List<PC> basketItems = new List<PC>();
+
+            foreach (Basket b in basket)
+            {
+                PC pc = os.GetPCDetails(b);
+                basketItems.Add(pc);
+
+                orderTotal += pc.Price * pc.Quantity;
+            }
+
+            Order order = new Order();
+            order.ItemsOrdered = basketItems;
+            order.UserID = user;
+            order.OrderDate = DateTime.Now;
+            order.TotalCost = (double)orderTotal;
+
+            return order;
+        }
+    }
+}
